1ab6739932968e38e64d663f13e4123ff934a69d
SathvikSesha <lakshmisarmatk@gmail.com>
2026-07-16 16:46:40 +0530

    Week 2

---
+++ Week2/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,22 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+	private BookRepository bookRepository;
+
+	// Setter for Dependency Injection
+	public void setBookRepository(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+
+	public void addBook() {
+
+		System.out.println("Book Service Called");
+
+		bookRepository.display();
+
+	}
+
+}
