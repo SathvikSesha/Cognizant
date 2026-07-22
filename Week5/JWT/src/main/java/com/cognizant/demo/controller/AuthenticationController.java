6600b6d2e1132afcaf69e59646684d05fd5b7982
SathvikSesha <lakshmisarmatk@gmail.com>
2026-07-22 12:26:54 +0530

    Spring security

---
+++ Week5/JWT/src/main/java/com/cognizant/demo/controller/AuthenticationController.java
@@ -0,0 +1,25 @@
+package com.cognizant.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(AuthenticationController.class);
+
+    @GetMapping("/authenticate")
+    public String authenticate() {
+
+        LOGGER.info("Start");
+
+        LOGGER.info("End");
+
+        return "Authentication Controller Working";
+
+    }
+
+}
